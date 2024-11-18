e111d95ffba31ce6378c32c02db14ea0e4547f5e
Edmundo Nascimento <edmundosony@hotmail.com>
2024-11-18 16:10:06 -0300

    chore: general config inside main components

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+//import "./services/api-mock";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
+
+//
+
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
